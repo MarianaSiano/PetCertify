605d9f987b9522dd3475450d7449eca69a122db7
MarianaSiano <marianasianop@gmail.com>
2026-04-04 11:15:15 -0300

    imports do tamplate do certificado feitos

---
+++ PetCertify/src/Components/CertficateTemplate.tsx
@@ -0,0 +1,5 @@
+import React, { useRef } from 'react';
+import { PetCertificateData } from '../types';
+import { Award, PawPrint, Star, Trophy, GraduationCap, Calendar, User, Dog } from 'lucide-react';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
