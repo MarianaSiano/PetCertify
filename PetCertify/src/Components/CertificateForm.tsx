36b4fde17f4be8a0d4ecab0cfacdd65526f026c7
MarianaSiano <marianasianop@gmail.com>
2026-04-04 15:01:55 -0300

    tipo de interface do form

--- PetCertify/src/Components/CertificateForm.tsx
+++ PetCertify/src/Components/CertificateForm.tsx
@@ -2,3 +2,6 @@ import React, { useState } from 'react';
 import { PetCertificateData, CertificateType } from '../types';
 import { PlusCircle, User, Dog, BookOpen, Star, Award, Trophy, Calendar } from 'lucide-react';
 
+interface CertificateFormProps {
+    onSubmit: (data: PetCertificateData) => void
+}
