d3036ab9c7910f6e7b43a8ce980f66434f802fbd
MarianaSiano <marianasianop@gmail.com>
2026-04-04 10:15:50 -0300

    interface do CSV concluida

--- PetCertify/src/Components/CSVUpload.tsx
+++ PetCertify/src/Components/CSVUpload.tsx
@@ -3,3 +3,7 @@ import { useDropzone } from 'react-dropzone';
 import Papa from 'papaparse';
 import { PetCertificateData, CertificateType } from '../types';
 import { Upload, FileText, AlertCircle } from 'lucide-react';
+
+interface CSVUploadProps {
+    onDataParsed: (data: PetCertificateData[]) => void;
+}
